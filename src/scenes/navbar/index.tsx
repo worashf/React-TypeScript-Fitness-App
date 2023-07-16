5c62c1e58d8ef08215bf5758f2db304a7259f38c
worashf <uppert83@gmail.com>
2023-07-16 16:34:36 +0300

    Add typescript and vite config

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,41 @@
+
+import  {useEffect, useState} from "react"
+import { Bars3Icon ,XMarkIcon} from "@heroicons/react/24/solid"
+
+import Logo  from "@/assets/Logo.png"
+type Props  ={}
+
+const Navbar = (props :Props) => {
+
+    const flexBetween =  "flex items-center  justify-between"
+  return (
+<nav>
+    <div className= {`${flexBetween} fixed top-0  z-30 py-6 w-full`}>
+        <div  className={`${flexBetween}  mx-auto w-5/6`} >
+         <div className={`${flexBetween} w-full gap-16`}>
+            {/* Left side  */}
+            <img src={Logo}  alt ="Logo"/>
+            {/* Right side links */}
+            <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8  text-sm`}>
+                    <p> Home</p>
+                    <p> Benefits</p>
+                    <p> Our Classes</p>
+                    <p> Contact Us</p>
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                    <p>Sign in</p>
+                    <button>Be come a Member</button>
+                </div>
+
+            </div>
+
+         </div>
+        </div>
+    </div>
+</nav>
+  )
+}
+
+export default Navbar
+
