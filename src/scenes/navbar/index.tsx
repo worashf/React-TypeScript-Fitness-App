acc7663f7046698764ad98ad2161aeb11333eb1d
worashf <uppert83@gmail.com>
2023-07-24 23:09:41 +0300

    Add responive navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,40 +1,68 @@
 
-import  {useEffect, useState} from "react"
-import { Bars3Icon ,XMarkIcon} from "@heroicons/react/24/solid"
+import { useEffect, useState } from "react"
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
+import Logo from "@/assets/Logo.png"
+import Link from "@/scenes/navbar/Link"
+import { SelectedPage } from "@/shared/types"
+import useMediaQuery from "@/hooks/useMediaQuery"
+type Props = {
+    isTopOfPage: boolean;
+    selectedPage: SelectedPage;
+    setSelectedPage: (value: SelectedPage) => void;
+};
 
-import Logo  from "@/assets/Logo.png"
-type Props  ={}
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
 
-const Navbar = (props :Props) => {
+    const flexBetween = "flex items-center  justify-between"
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    return (
+        <nav>
+            <div className={`${flexBetween} fixed top-0  z-30 py-6 w-full`}>
+                <div className={`${flexBetween}  mx-auto w-5/6`} >
+                    <div className={`${flexBetween} w-full gap-16`}>
+                        {/* Left side  */}
+                        <img src={Logo} alt="Logo" />
+                        {/* Right side links */}
+                        {isAboveMediumScreens ? (
+                            <div className={`${flexBetween} w-full`}>
+                                <div className={`${flexBetween} gap-8  text-sm`}>
+                                    <Link page="Home"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage} />
+                                    <Link page="Benefits"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage} />
+                                    <Link
+                                        page="Our Classes"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                    <Link
+                                        page="Contact Us"
+                                        selectedPage={selectedPage}
+                                        setSelectedPage={setSelectedPage}
+                                    />
+                                </div>
+                                <div className={`${flexBetween} gap-8`}>
+                                    <p>Sign in</p>
+                                    <button>Be come a Member</button>
+                                </div>
 
-    const flexBetween =  "flex items-center  justify-between"
-  return (
-<nav>
-    <div className= {`${flexBetween} fixed top-0  z-30 py-6 w-full`}>
-        <div  className={`${flexBetween}  mx-auto w-5/6`} >
-         <div className={`${flexBetween} w-full gap-16`}>
-            {/* Left side  */}
-            <img src={Logo}  alt ="Logo"/>
-            {/* Right side links */}
-            <div className={`${flexBetween} w-full`}>
-                <div className={`${flexBetween} gap-8  text-sm`}>
-                    <p> Home</p>
-                    <p> Benefits</p>
-                    <p> Our Classes</p>
-                    <p> Contact Us</p>
+                            </div>) : (
+                            <button
+                                className="rounded-full bg-secondary-500 p-2"
+                                onClick={() => setIsMenuToggled(!isMenuToggled)}
+                            >
+                                <Bars3Icon className="h-6 w-6 text-white" />
+                            </button>
+                        )}
+
+                    </div>
                 </div>
-                <div className={`${flexBetween} gap-8`}>
-                    <p>Sign in</p>
-                    <button>Be come a Member</button>
-                </div>
-
             </div>
-
-         </div>
-        </div>
-    </div>
-</nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
