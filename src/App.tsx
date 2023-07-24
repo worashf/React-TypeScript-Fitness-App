025c1b77558b015af26566278123083a1c33a110
worashf <uppert83@gmail.com>
2023-07-24 23:10:40 +0300

    Add props in the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 
 import Navbar from "@/scenes/navbar"
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
-
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
   return (
     <div className='app bg-gray-20'>
-      <Navbar />
+      <Navbar isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
